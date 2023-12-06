f0ef8b18176ec0b14e92385dfdabc75724fce454
norakaymakcilar <nora.k@outlook.fr>
2023-12-06 15:05:43 +0100

    feat :Implémentation de 4 robots

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/robots/Robots.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/robots/Robots.java
@@ -1,5 +1,6 @@
 package fr.cotedazur.univ.polytech.startingpoint.robots;
 
+import fr.cotedazur.univ.polytech.startingpoint.characters.CharactersType;
 import fr.cotedazur.univ.polytech.startingpoint.characters.ListCharacters;
 import fr.cotedazur.univ.polytech.startingpoint.complements.Strategies;
 import fr.cotedazur.univ.polytech.startingpoint.districts.DistrictsType;
@@ -11,18 +12,20 @@ public class Robots {
     private int number;
     private int golds;
     private Strategies strategies;
-    private DistrictsType district;
+
+    private List<DistrictsType> district;
+    private CharactersType character;
     private ListCharacters listCharacters;
 
-    public Robots(int number, DistrictsType district, List<ListCharacters> listCharacters, Strategies strategy, int golds) {
+    public Robots(int number, List<DistrictsType> district, CharactersType character, Strategies strategy, int golds) {
         this.number = number;
         this.district = district;
         this.golds = golds;
-        this.listCharacters = getRandomCharacter(listCharacters);
+        this.character = character;
     }
 
-    public Robots(int number, DistrictsType district, List<ListCharacters> listCharacters, Strategies strategy) {
-        this(number, district, listCharacters, strategy, 2);
+    public Robots(int number, List<DistrictsType> district, CharactersType character, Strategies strategy) {
+        this(number, district, character, strategy, 2);
     }
 
     public int getNumber() {
@@ -37,30 +40,25 @@ public class Robots {
         return strategies;
     }
 
-    public DistrictsType getDistrict() {
-        return district;
-    }
-
-    public ListCharacters getListCharacters() {
-        return listCharacters;
-    }
 
     public void setGolds(int golds) {
         this.golds = golds;
     }
 
-    public void setDistrict(DistrictsType district) {
+    public void setDistrict(List<DistrictsType> district) {
         this.district = district;
     }
 
 
-    private ListCharacters getRandomCharacter(List<ListCharacters> listCharacters) {
-        if (listCharacters != null && !listCharacters.isEmpty()) {
-            int randomIndex = (int) (Math.random() * listCharacters.size());
-            return listCharacters.get(randomIndex);
-        }
-        return null;
+    public void setDistricts(List<DistrictsType> districtsForBot) {
+        this.district = districtsForBot;
     }
+
+
+    public void setCharacter(CharactersType character) {
+        this.character = character;
+    }
+
 }
 
 
