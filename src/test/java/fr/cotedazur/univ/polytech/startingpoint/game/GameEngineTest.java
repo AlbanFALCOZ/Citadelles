afe9a02a0975a800a287d6b6a30dbb1b12765250
Sara-tfq <147254110+Sara-tfq@users.noreply.github.com>
2023-12-07 17:16:31 +0100

    Ajour d'une méthode qui renvoie la liste des robots triés par le numéro de leur carte personnage

--- src/test/java/fr/cotedazur/univ/polytech/startingpoint/game/GameEngineTest.java
+++ src/test/java/fr/cotedazur/univ/polytech/startingpoint/game/GameEngineTest.java
@@ -6,7 +6,9 @@ import fr.cotedazur.univ.polytech.startingpoint.robots.Robot;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -43,6 +45,19 @@ class GameEngineTest {
         //si les robots ont 4 golds après le tour.
         for (Robot bot : gameEngine.getBots()) {
             assertEquals(4, bot.getGolds(), "Le robot doit avoir 4 golds après un tour.");
+        }
+    }
+
+    @Test
+    public void testSortRobots() {
+        GameEngine gameEngine = new GameEngine();
+        gameEngine.initializeBots();
+        gameEngine.assignRandomCharacterToRobots();
+        List<Robot> originalOrder = new ArrayList<>(gameEngine.getBots());
+        ArrayList<Robot> sortedRobots = gameEngine.sortRobots();
+        assertEquals(originalOrder, gameEngine.getBots());
+        for (int i = 1; i < sortedRobots.size(); i++) {
+            assertTrue(sortedRobots.get(i - 1).getCharacter().getNumber() <= sortedRobots.get(i).getCharacter().getNumber());
         }
     }
 }
