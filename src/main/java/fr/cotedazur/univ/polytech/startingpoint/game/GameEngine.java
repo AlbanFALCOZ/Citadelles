4fe827b320927e4b3456e2a4f10a8da857fc8184
ADZAHO-Selom-Ami <selom-ami.adzaho@etu.unice.fr>
2023-12-08 14:53:29 +0100

    refactor: modification de la classe principale du pom.xml

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/game/GameEngine.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/game/GameEngine.java
@@ -50,7 +50,7 @@ public class GameEngine {
     public void assignCrown(){
         Collections.shuffle(bots);
         bots.get(0).setHasCrown(true);
-        System.out.println(bots.get(0).getName() + " has crown");
+        System.out.println(bots.get(0).getName() + " has crown and start the call of the characters");
         Collections.sort(bots, Comparator.comparingInt(bot -> bot.getCharacter().getNumber()));
 
     }
