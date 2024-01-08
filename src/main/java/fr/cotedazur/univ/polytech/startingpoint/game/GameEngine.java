d0d36889c82f6dcba81d4751b4254cc099094892
Alban FALCOZ <alban.falcoz@orange.fr>
2024-01-08 08:08:44 +0100

    feat:  Pouvoir effectuer un grand nombre de parties à la suite #53

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/game/GameEngine.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/game/GameEngine.java
@@ -14,14 +14,19 @@ public class GameEngine {
     private DeckDistrict deckDistricts;
     private DeckCharacters deckCharacters;
     private Round round;
+    private boolean systemPrint = false;
     
-    public GameEngine() {
+    public GameEngine(boolean systemPrint) {
+        this.systemPrint = systemPrint;
         deckDistricts = new DeckDistrict();
         deckCharacters = new DeckCharacters();
         this.bots = new ArrayList<>();
         round = new Round(bots);
         initializeBots();
+    }
 
+    public GameEngine() {
+        this(true);
     }
 
     public void initializeBots() {
@@ -48,14 +53,14 @@ public class GameEngine {
         for (Robot bot : bots ){
             if(bot.getHasCrown()){
                 bot.setCharacter(ListCharacters.get(0));
-                System.out.println(bot.getName() +" With crown Picked " +ListCharacters.get(0).getColor() + ListCharacters.get(0).getType() + bot.getRESET());
+                if (systemPrint) System.out.println(bot.getName() +" With crown Picked " +ListCharacters.get(0).getColor() + ListCharacters.get(0).getRole() + bot.getRESET());
                 ListCharacters.remove(ListCharacters.get(0));
             }
         }
         for (Robot bot : bots){
             if(!bot.getHasCrown()){
                 bot.setCharacter(ListCharacters.get(i));
-                System.out.println(bot.getName() +" Picked " +ListCharacters.get(i).getColor() + ListCharacters.get(i).getType() + bot.getRESET());
+                if (systemPrint) System.out.println(bot.getName() +" Picked " +ListCharacters.get(i).getColor() + ListCharacters.get(i).getRole()  + bot.getRESET());
                 i++;
             }
         }
@@ -64,7 +69,7 @@ public class GameEngine {
     public void assignCrown(){
         Collections.shuffle(bots);
         bots.get(0).setHasCrown(true);
-        System.out.println(bots.get(0).getName() + " has crown and start the call of the characters");
+        if (systemPrint) System.out.println(bots.get(0).getName() + " has crown and start the call of the characters");
         Collections.sort(bots, Comparator.comparingInt(bot -> bot.getCharacter().getNumber()));
 
     }
@@ -96,22 +101,23 @@ public class GameEngine {
         round = new Round(bots);
         String turnStarting = "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++Turn ";
         String turnEnding = "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n";
-        System.out.println("=============================================================================GAME IS STARTING====================================================================\n");
+        if (systemPrint) System.out.println("=============================================================================GAME IS STARTING====================================================================\n");
         int comptTurn = 1;
         assignRandomCharacterToRobots();
         assignCrown();
         robotsPickCharacters();
         while(!isBuiltEigthDistrict() && comptTurn++>=1){
-            System.out.println(turnStarting + comptTurn + " is starting" + turnEnding);
+            if (systemPrint) System.out.println(turnStarting + comptTurn + " is starting" + turnEnding);
             bots.sort(Comparator.comparingInt(bot -> bot.getCharacter().getNumber()));
             for (Robot bot : bots) {
                 if (bot.getHasCrown()) {
-                    System.out.println(bot.getName() + " has crown and start the call of the characters");
+                    if (systemPrint) System.out.println(bot.getName() + " has crown and start the call of the characters");
                 }
             }
             robotsPickCharacters();
-            System.out.println(round.playTurns());
-            System.out.println(turnStarting + comptTurn + " is over" + turnEnding);
+            if (!systemPrint) round.playTurns();
+            else System.out.println(round.playTurns());
+            if (systemPrint) System.out.println(turnStarting + comptTurn + " is over" + turnEnding);
             comptTurn++;
             round = new Round(bots);
         }
@@ -132,7 +138,7 @@ public class GameEngine {
         for (int i = 0; i < 3; i++) {
             if (!charactersInHand.isEmpty()) {
                 CharactersType destroyedCharacter = charactersInHand.remove(0);
-                System.out.println("Destroyed character: " + destroyedCharacter.getColor() + destroyedCharacter.getType() + bots.get(0).getRESET());
+                if (systemPrint) System.out.println("Destroyed character: " + destroyedCharacter.getColor() + destroyedCharacter.getRole() + bots.get(0).getRESET());
             }
         }
         charactersInHand.add(CharactersType.ROI);
--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/game/Main.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/game/Main.java
@@ -3,19 +3,46 @@ package fr.cotedazur.univ.polytech.startingpoint.game;
 
 import fr.cotedazur.univ.polytech.startingpoint.robots.Robot;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 import java.util.List;
 
 public class Main {
 
-    public static void main (String[] args) {
+    public static void main (String[] args) throws FileNotFoundException {
 
+        long start = System.currentTimeMillis();
+        int[] listWinners = new int[4];
+// ...
+        for (int i = 0; i < 1000; i++) {
+            GameEngine Game = new GameEngine(false);
+            Game.gameTurns();
+            Winner winner = new Winner(Game.getBots());
+            for (String winners : winner.getWinners()) {
+                if (winners.equals("Nora")) {
+                    listWinners[0]++;
+                }
+                if (winners.equals("Stacy")) {
+                    listWinners[1]++;
+                }
+                if (winners.equals("Alban")) {
+                    listWinners[2]++;
+                }
+                if (winners.equals("Sara")) {
+                    listWinners[3]++;
+                }
+            }
+            //System.out.println(winner.calculateScores());
+            //System.out.println(winner.showWinners());
+        }
 
-        GameEngine Game = new GameEngine();
-        Game.gameTurns();
-        Winner winner = new Winner(Game.getBots());
-        System.out.println(winner.calculateScores());
-        System.out.println(winner.showWinners());
-
+        for (int i = 0; i < listWinners.length; i++) {
+            System.out.println("Winner N°" + i + " : " + listWinners[i]);
+        }
+        long finish = System.currentTimeMillis();
+        long timeElapsed = finish - start;
+        System.out.println(timeElapsed/1000 + "sec");
 
     }
 
