d251048f1ed18f911efc880fd17bccfde155416c
ADZAHO-Selom-Ami <selom-ami.adzaho@etu.univ-cotedazur.fr>
2024-02-05 17:17:55 +0100

    feat:ajout du fichier .csv #114

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/gamestats/ParseFullGameStats.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/gamestats/ParseFullGameStats.java
@@ -1,23 +1,22 @@
 package fr.cotedazur.univ.polytech.startingpoint.gamestats;
 import com.opencsv.CSVReader;
+
+import java.io.File;
 import java.io.FileReader;
-import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
 
 public class ParseFullGameStats {
-    @SuppressWarnings("resource")
-    public static void main(String[] args) throws Exception {
+    public static void parseFullStats() throws Exception {
 
-        URL fileUrl = ParseGameStatsByLine.class.getClassLoader().getResource("stats/.csv");
+        File file = new File("stats/gamestats.csv");
 
-        //Build reader instance
-        CSVReader reader = new CSVReader(new FileReader(fileUrl.getFile()));
+        List<String[]> allRows;
+        try (CSVReader reader = new CSVReader(new FileReader(file))) {
 
-        //Read all rows at once
-        List<String[]> allRows = reader.readAll();
+            allRows = reader.readAll();
+        }
 
-        //Read CSV line by line and use the string array as you want
         for (String[] row : allRows) {
             System.out.println(Arrays.toString(row));
         }
